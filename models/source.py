2b1b3a54280f4c7c6de48233b998ced4dd9dfeb8
shenhui0509 <shlvhff@163.com>
2016-01-15 12:04:37 +0800

    modified source.py

--- models/source.py
+++ models/source.py
@@ -1,20 +1,21 @@
 import simpy
 from numpy import random
+from unit import Message
+from msg_queue import MsgQueue
 
-discrete_distributions = {
-    'binomial' : random.binomial,
-    'geometric' : random.geometric,
-    'hypergeometric' : random.hypergeometric
-}
+discrete_distributions = dict(binomial=random.binomial, geometric=random.geometric)
 
-class source(object):
-    '''
+
+class Source(object):
+    """
     source generator in queueing theory
     generates customers as a given process
     default is Possion process with lambda = 1
     Usage: source(env, gen_rate = 1,compound = False, comp_dist = None, comp_dist_params = None)
-    '''
+    """
+
     def __init__(self, env, **kwargs):
+        assert (isinstance(env, simpy.Environment()))
         self.__env = env
 
         if 'gen_rate' in kwargs:
@@ -25,7 +26,25 @@ class source(object):
         if 'compound' in kwargs:
             self.__compound = kwargs['compound']
             if self.__compound:
-                assert(kwargs['comp_dist'] is not None)
-                assert(kwargs['comp_dist_params'] is not None)
+                assert (kwargs['comp_dist'] is not None)
+                assert (kwargs['comp_dist_params'] is not None)
                 self.__comp_dist = discrete_distributions[kwargs['comp_dist']]
                 self.__comp_dist_params = kwargs['comp_dist_params']
+
+        self.action = self.__env.process(self.run())
+        self.__dst_server = None
+
+    def run(self):
+        assert (self.__dst_server is not None)
+        while True:
+            interval = random.exponential(self.__gen_rate)
+            packet_num = self.__comp_dist(self.__comp_dist_params)
+            msg = Message(self.__env, packet_num)
+            self.sendto(self.__dst, msg)
+            yield self.__env.timeout(interval)
+
+    def sendto(self, dst, msg):
+        assert isinstance(dst, MsgQueue)
+        assert isinstance(msg, Message)
+        dst.on_arrival(msg)
+
